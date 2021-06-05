27f1bde1bb6d77ddd78aabd4303d7b44049aa104
ZCXXVII <84192514+zcxxvii@users.noreply.github.com>
2021-06-05 10:27:29 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -149,6 +149,11 @@ class MirrorListener(listeners.MirrorListeners):
     def onUploadComplete(self, link: str, size, files, folders, typ):
         with download_dict_lock:
             msg = f'\n<b>GDrive link:</b> <a href="{link}">{download_dict[self.uid].name()}</a> <b>({download_dict[self.uid].size()})</b>'
+                msg += '\n\n<b>Type: </b>Folder'
+                msg += f'\n<b>SubFolders: </b>{folders}'
+                msg += f'\n<b>Files: </b>{files}'
+            else:
+                msg += f'\n\n<b>Type: </b>{typ}'
             """
             msg = f'<b>Filename: </b><code>{download_dict[self.uid].name()}</code>\n<b>Size: </b><code>{size}</code>'
             buttons = button_build.ButtonMaker()
